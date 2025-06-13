e14138aac142dc043f31280fcf0cdaaa45abf03e
Mike Jensen <jent@threadly.org>
2025-06-12 20:45:17 -0600

    test: Additional tests for small helper functions

--- chartdraw/drawing/stack_graphic_context_test.go
+++ chartdraw/drawing/stack_graphic_context_test.go
@@ -4,7 +4,11 @@ import (
 	"math"
 	"testing"
 
+	"github.com/golang/freetype/truetype"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/go-analyze/charts/chartdraw/roboto"
 )
 
 func TestStackGraphicContextSaveRestore(t *testing.T) {
@@ -59,4 +63,39 @@ func TestStackMatrixTransform(t *testing.T) {
 	x, y := got.TransformPoint(0, 0)
 	assert.InDelta(t, 5.0, x, 0.0001)
 	assert.InDelta(t, 7.0, y, 0.0001)
+}
+
+func TestStackComposeMatrixTransform(t *testing.T) {
+	t.Parallel()
+
+	gc := NewStackGraphicContext()
+	gc.SetMatrixTransform(NewTranslationMatrix(5, 7))
+	gc.ComposeMatrixTransform(NewTranslationMatrix(3, 4))
+	got := gc.GetMatrixTransform()
+	x, y := got.TransformPoint(0, 0)
+	assert.InDelta(t, 8.0, x, 0.0001)
+	assert.InDelta(t, 11.0, y, 0.0001)
+}
+
+func TestStackLineDash(t *testing.T) {
+	t.Parallel()
+
+	gc := NewStackGraphicContext()
+	dash := []float64{1, 2, 3}
+	gc.SetLineDash(dash, 0.5)
+	assert.Equal(t, dash, gc.current.Dash)
+	assert.InDelta(t, 0.5, gc.current.DashOffset, 0.0001)
+}
+
+func TestStackFontRoundTrip(t *testing.T) {
+	t.Parallel()
+
+	f, err := truetype.Parse(roboto.Roboto)
+	require.NoError(t, err)
+
+	gc := NewStackGraphicContext()
+	gc.SetFont(f)
+	gc.SetFontSize(13.0)
+	assert.Equal(t, f, gc.GetFont())
+	assert.InDelta(t, 13.0, gc.GetFontSize(), 0.0001)
 }
